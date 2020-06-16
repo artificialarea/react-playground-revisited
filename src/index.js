81e1151a4d501dae5dc9c472aeacc5710a598802
Sacha Sedriks <sachased@gmail.com>
2020-06-16 14:27:16 -0700

    update commentary re: mountNode variable.

--- src/index.js
+++ src/index.js
@@ -8,6 +8,12 @@ let mountNode = document.getElementById('root');
 ReactDOM.render(
   <App />,
   // document.getElementById('root')
-  mountNode // but why?
+  mountNode 
 );
 
+// re: mountNode
+// src: https://www.freecodecamp.org/news/all-the-fundamental-react-js-concepts-jammed-into-this-single-medium-article-c83f9b53eac2/
+
+// "The second argument to ReactDOM.render is the destination DOM element which React is going to take over and control. In the jsComplete React Playground, you can just use the special variable 'mountNode'."
+
+// presumably their variable mountNode has been assigned document.getElementById('root') ??
