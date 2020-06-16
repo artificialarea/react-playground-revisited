be811b70b995a48f24dc74fff4ee268edf76fd05
Sacha Sedriks <sachased@gmail.com>
2020-06-16 16:01:12 -0700

    moved components into a directory.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './m-drill-1/App';
 
 let mountNode = document.getElementById('root');
 
