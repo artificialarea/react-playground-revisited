b428dc263079f4e272e282d2b207a17c9d2794f2
Sacha Sedriks <sachased@gmail.com>
2020-06-15 21:49:18 -0700

    integrated snapshot with react-test-renderer... but can't test interaction with event handlers, will use enzyme adapter instead (npm installed + configured in setupTests.js.

--- src/Tabs.test.js
+++ src/Tabs.test.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
+import { shallow } from 'enzyme'; // NOTE: requires configuration set up in setupTests.js
 import Tabs from './Tabs';
+import tabstore from './tabstore';
 
 describe('<Tabs />', () => {
+
+  // mimicing state from App.js
+  const tabsProp = [ ...tabstore ] 
+
   it('renders without crashing', () => {
     const div = document.createElement('div')
     ReactDOM.render(<Tabs />, div)
     ReactDOM.unmountComponentAtNode(div)
   });
-  
+
+  // Unfortunately, can't sufficiently test event listeners with 'react-test-renderer'. Need to use 'Enzyme' testing library.
+  it('renders UI as expected', () => {
+    const tree = renderer
+      .create(<Tabs tabs={tabsProp}/>)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  // ENZYME testing of event handlers
+  // NOTE: requires configuration set up in setupTests.js
+  it('renders empty given no tabs', () => {
+
+  })
+
 });
--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,9 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
+
+// Enzyme testing library for testing event handlers (Tab.test.js)
+// $ npm install enzyme enzyme-adapter-react-16 --save-dev
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+configure({ adapter: new Adapter() });
