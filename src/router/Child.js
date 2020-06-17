9dc0edce441413ba230ca274bb3979ca321a9ffc
Sacha Sedriks <sachased@gmail.com>
2020-06-17 10:21:33 -0700

    added react-router-dom <Redirect /> logic in Child.js to Error.js if dynamic path is bogus '/child/:not-ChildId'.

--- src/router/Child.js
+++ src/router/Child.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Redirect } from 'react-router-dom'
 
 export default function Child(props) {
   console.log(props); // note it packages 'list' prop into route render props!
@@ -8,7 +9,12 @@ export default function Child(props) {
   const child = list.find(item => 
     item.id === props.match.params.childId
   )
-
+  
+  // to ensure app doesn't crash but redirects to error route
+  // if path '/child/:not-childId' (and thus childId is 'undefined')
+  if(!child) {
+    return <Redirect to='/child-error'/>
+  }
   return (
     <div className='child'>
       <h2>{child.id} Componenent</h2>
