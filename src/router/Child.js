2b0284814ea29e3d90b91bd6435c591d0e2c4908
Sacha Sedriks <sachased@gmail.com>
2020-06-17 11:51:35 -0700

    added react-router-dom Redirect for dynamic route path error: 1min.

--- src/router/Child.js
+++ src/router/Child.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import { Redirect } from 'react-router-dom'
 
 export default function Child(props) {
   const theChild = props.children.find(child => (
     child.id === props.match.params.childId
   ))
+
+  if (!theChild) {
+    return <Redirect to='/error'/>
+  }
   return (
     <div className={`child child__${theChild.id}`}>
       <h2>{theChild.id} Component</h2>
