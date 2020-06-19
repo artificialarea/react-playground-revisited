55bcbab04ed6c78f7528ad964e99cfa769d1f237
Sacha Sedriks <sachased@gmail.com>
2020-06-19 09:10:50 -0700

    updated template and README instructions.

--- src/states/StatefulApp.js
+++ src/states/StatefulApp.js
@@ -1,49 +1,11 @@
 import React from 'react';
 import './StatefulApp.css';
-import StatelessButton from './StatelessButton';
-import { PropTypes } from 'prop-types'
+
 
 export default class StatefulApp extends React.Component {
-  
-  constructor(props) {
-    super(props)
-    this.state = {
-      isLiked: false,
-      count: 0
-    }
-  }
-
-  handleUpdate = () => {
-    const { isLiked, count } = this.state;
-    const newCount = count + 1;
-    this.setState({
-      isLiked: !isLiked,
-      count: newCount
-    })
-  }
-
-
   render() {
-    const { isLiked, count } = this.state
     return (
-      <div className="stateful">
-        <h2>{this.props.headline}</h2>
-        <p className='emoji'>{ isLiked ? '👍' : '👎' }</p>
-        <p>Click Counter: <b>{count}</b></p>
-
-        <StatelessButton 
-          state={this.state}
-          clickUpdate={() => this.handleUpdate()}
-        />
-      </div>
+      <div></div>
     )
   }
-}
-
-StatefulApp.defaultProps = {
-  headline: 'Like Stateful?'
-}
-
-StatefulApp.propTypes = {
-  headline: PropTypes.string
 }
--- src/states/StatelessButton.js
+++ src/states/StatelessButton.js
@@ -2,15 +2,7 @@ import React from 'react'
 
 
 export default function StatelessButton(props) {
-  const { isLiked } = props.state;
-  // console.log(props)
   return (
-    <>
-      <button
-        onClick={() => props.clickUpdate()}
-      >
-        {isLiked ? 'dislike?' : 'like?'}
-      </button>
-    </>
+    <div></div>
   )
 }
