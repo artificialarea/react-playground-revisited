ba71821e1eefdb11e0ea41d1a97dac7584add5f5
Sacha Sedriks <sachased@gmail.com>
2020-06-19 11:23:38 -0700

    finished in ~11min.

--- src/states/StatefulApp.js
+++ src/states/StatefulApp.js
@@ -1,11 +1,39 @@
 import React from 'react';
 import './StatefulApp.css';
+import StatelessButton from './StatelessButton';
 
 
 export default class StatefulApp extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      isLiked: false,
+      counter: null
+    }
+  }
+
+  handleClick = () => {
+    const newCounter = this.state.counter + 1
+    this.setState({
+      isLiked: !this.state.isLiked,
+      counter: newCounter
+    })
+  }
+
   render() {
+    const { isLiked, counter } = this.state
     return (
-      <div></div>
+      <div>
+        <h2>Like Statefulness?</h2>
+        <p>{ isLiked ? '👍' : '👎'}</p>
+        <p>click counter: {counter}</p>
+        <StatelessButton 
+          {...this.state}
+          word='like?'
+          handleClick={this.handleClick}
+        />
+      </div>
     )
   }
 }
--- src/states/StatelessButton.js
+++ src/states/StatelessButton.js
@@ -1,8 +1,24 @@
 import React from 'react'
-
+import { PropTypes } from 'prop-types'
 
 export default function StatelessButton(props) {
+  
   return (
-    <div></div>
+    <button
+      onClick={() => props.handleClick()}
+    >
+      { !props.isLiked ? props.word : 'dislike?'}
+    </button>
   )
+}
+
+
+StatelessButton.defaultProps = {
+  word: 'test',
+  isLiked: false,
+}
+
+StatelessButton.propTypes = {
+  word: PropTypes.string,
+  isLiked: PropTypes.bool
 }
